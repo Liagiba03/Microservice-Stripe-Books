b75651b3991d27668bb3926b18ad41523624e57f
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-12-09 20:46:38 -0600

    Add crud endpoints to products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { CreateProductDto, UpdateProductDto } from './dto/products.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Post('create-product')
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.createProduct(createProductDto);
+  }
+
+  @Patch('update-product/:id')
+  updateCustomer(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.updateProduct(id, updateProductDto);
+  }
+
+  @Delete('delete-product/:id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productsService.deleteProduct(id);
+  }
+
+  @Get('show-product/:id')
+  getProductDetails(@Param('id') productId: string) {
+    return this.productsService.getProductDetails(productId);
+  }
+
+  @Get('show-all-products')
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+
 }
